0ce56fab12623c3e425c7621700412bfd0e820d2
Zhaoyu <weixiaobo88@gmail.com>
2019-07-22 09:13:30 +0800

    finish refactor

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,9 @@
 package com.tws.refactoring;
 
 public class Police {
+    private int AGE = 18;
+
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
+        return driver.getAge() >= AGE;
     }
 }
